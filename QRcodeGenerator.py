0c4e89e51384ed73617c95f99e941eee6a69d7f7
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-24 15:21:06 +0100

    to get input from user

--- QRcodeGenerator.py
+++ QRcodeGenerator.py
@@ -1,9 +1,9 @@
-# importing the neccessary library using pip install qrcode Image
+# importing the neccessary library using pip install qrcode Image.
 import qrcode
 
-# Creating a function that collects a text and then converts it to a qrcode
-def generate_qrcode(text):
 
+# Creating a function that collects a text and then converts it to a qrcode.
+def generate_qrcode(text):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -13,6 +13,11 @@ def generate_qrcode(text):
     )
     qr.add_data(text)
     qr.make(fit=True)
+    # save the qr code as an image.
     img = qr.make_image(fill_color="black", back_color="white")
     img.save("qr_image01")
 
+
+# get input text from user
+url = input("Enter your url: ")
+generate_qrcode(url)
